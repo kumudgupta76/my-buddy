ae28cc88d8dd558f988a0833a7ec01089fdae710
Kumud Gupta <40192898+kumudgupta76@users.noreply.github.com>
2024-08-08 15:20:00 +0000

    update theam color

--- src/components/timer/CountdownTimer.jsx
+++ src/components/timer/CountdownTimer.jsx
@@ -66,12 +66,14 @@ const CountdownTimer = () => {
     };
 
     const playSound = () => {
+        console.log("Play Sound start", new Date().toLocaleDateString());
         const newData = [{ time: mins, completedAt: new Date().toLocaleString() }, ...data];
         setData(newData);
         const audio = new Audio('/my-buddy/assets/simple-notification-152054.mp3'); // Path to your sound file
         audio.play().catch(error => {
             console.error('Error playing sound:', error);
         });
+        console.log("Play Sound end", new Date().toLocaleDateString());
     };
 
     const openNotification = () => {
